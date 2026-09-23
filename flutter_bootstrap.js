095cfec00bbacec4867ab10753e79fc55048f7de
sumanranaa25-ui <sumanranaa25@gmail.com>
2026-09-23 13:37:36 +0000

    onbhresd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989953651" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "942481228" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
